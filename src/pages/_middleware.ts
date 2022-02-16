d6258a3b59b28b639ae3bac156baf2de95df87aa
Aries <10256477+ariesclark@users.noreply.github.com>
2022-02-15 23:20:29 -0700

    😅😫

--- src/pages/_middleware.ts
+++ src/pages/_middleware.ts
@@ -3,7 +3,7 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export async function middleware (req: NextRequest) {
-	if (["/", "/hiring"].includes(req.nextUrl.pathname) {
+	if (["/", "/hiring"].includes(req.nextUrl.pathname]) {
 		const date = new Date();
 	
 		req.nextUrl.pathname = `/hiring/${date.getFullYear()}/${date.getMonth()}`;
