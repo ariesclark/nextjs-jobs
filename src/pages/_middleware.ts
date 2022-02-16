db5b159d538eeeae89f2f983aa553e1c2d362c5f
Aries <10256477+ariesclark@users.noreply.github.com>
2022-02-15 23:14:13 -0700

    Update _middleware.ts

--- src/pages/_middleware.ts
+++ src/pages/_middleware.ts
@@ -3,16 +3,12 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export async function middleware (req: NextRequest) {
-	const { pathname } = req.nextUrl;
-	const date = new Date();
-	const url = req.nextUrl.clone();
-	url.pathname = `/hiring/${date.getFullYear()}/${date.getMonth()}`;
+	if (["/", "/hiring"].includes(pathname) {
+		const date = new Date();
+	
+		req.nextUrl.pathname = `/hiring/${date.getFullYear()}/${date.getMonth()}`;
+		return NextResponse.redirect(url, 303);
+	}
 
-	if (pathname === "/") {
-		return NextResponse.redirect(url, 303);
-	}
-	if (pathname === "/hiring") {
-		return NextResponse.redirect(url, 303);
-	}
 	return NextResponse.next();
 }
